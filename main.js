3d66336e934f8919d04fe6e04eff871b48d84ee1
lypintopimiento <lypintopimiento@gmail.com>
2021-05-10 18:23:38 -0500

    Libreria_Matematica_30_04_2021

---
+++ main.js
@@ -0,0 +1,57 @@
+
+// Potencia segun la base y el exponente
+console.log(Math.pow(5,2));
+console.log(Math.SQRT2);
+
+// Cuanto equivale un  raiz=1/2
+console.log(Math.SQRT1_2);
+
+//Cuanto equivale PI
+console.log(Math.PI);
+
+
+// Mostrar el numero de decimales aproximados en 32bit pero en hexdecimal
+// let numero = 1.05;
+// console.log(Math.fround(numero));
+
+// // Numero aleatorios  
+// let i = 0;
+// do{
+//     let numero = Math.random()*100;
+//     console.log("Numero aleatorio",numero);
+//     console.log("Redondear cuando el decimal es mayor a 5",Math.round(numero));
+//     console.log("Redondear cuando el decimal es menor a 5",Math.floor(numero));
+//     console.log("Eliminar los decimales de un número",Math.trunc(numero));
+//     i++;
+// }while(i<50)
+// i = 0;
+
+// Convertir los parametros en un objeto
+// let funcioneObjetos = ({...objects})=>{
+//     console.log(objects);
+//     //Ejecutar la funcion almacenada en parametro 'objects'
+//     console.log(objects.saludar());
+// }
+// funcioneObjetos({id:123, name: "Marly", saludar : () => `Hola soy Marly` });
+
+// Convertir los parametros en un array numerico
+// let funcioneArray = (...p1)=>{
+//     console.log(p1);
+// }
+// funcioneArray(1,"Hola",true);
+
+//Buscar el numero menor de una array
+// let listanumeros = [1,33,68,14,7000];
+// console.log(Math.min(...listanumeros));
+
+//Buscar el numero mayor de una array
+// let listanumeros = [1,33,68,14,7000];
+// console.log(Math.max(...listanumeros));
+
+// Raiz cubica
+// let resultado = Math.cbrt(125);
+// console.log(resultado);
+
+// Raiz cuadrada
+// let resultado = Math.sqrt(5);
+// console.log(resultado);
